3876bf63f8d23338e224f5871473f443ccf90849
Hemraj Jibhau Solanki <siya21211@gmail.com>
2020-04-09 16:03:10 +0530

    first commit

---
+++ app/Http/Controllers/ItemApiController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Item;
+
+class ItemApiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Item::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function changeStatus(Request $request, $id)
+    {
+      $item = Item::find($id);
+      $item->status = $request->status;
+      $item->save();
+      return $item;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+          return Item::create($request->all());
+    }
+    
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $item = Item::find($id);
+        $item->update($request->all());
+        return $item;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+      $item = Item::find($id);
+      $item->delete();
+      return 204;
+    }
+}
---
+++ database/seeds/ItemSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ItemSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('items')->insert([
+            'name' => Str::random(10),
+            'description' => Str::random(10)
+        ]);
+    }
+}
